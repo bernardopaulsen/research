940047cdf62b214c9ed85ef37896616bbccb15aa
bernardopaulsen <ubuntu@ip-172-31-75-133.ec2.internal>
2021-02-02 22:51:02 +0000

    rand_port moved

---
+++ rand_port/nyse/1_tickers.py
@@ -0,0 +1,34 @@
+"""
+Title      : NYSE Tickers
+Description: Gets tickers of all stocks listed on the NYSE from Wikipedia page.
+Author     : Bernardo Paulsen
+Version    : 0.9.0
+"""
+
+
+import pickle
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+
+
+options = Options()
+options.add_argument('--headless')
+letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+with webdriver.Chrome("/Users/bernardopaulsen/chromedriver",options=options) as chrome:
+    tickers = []
+    for letter in letters:
+        print(letter)
+        chrome.get(f"https://en.wikipedia.org/wiki/Companies_listed_on_the_New_York_Stock_Exchange_({letter})")
+        time.sleep(.5)
+        table   = chrome.find_element(By.XPATH, "//table")
+        stocks  = table.find_elements(By.XPATH, "//td[2]")
+        for stock in stocks:
+            ticker = stock.text
+            tickers.append(ticker)
+        print(len(tickers))
+
+
+with (open("tickers.pickle","wb")) as file:
+    pickle.dump(tickers,file)
---
+++ rand_port/nyse/3_clean.py
@@ -0,0 +1,23 @@
+"""
+Title      : Clean Stocks
+Description: Removes stocks with less than the maximum data length from dataframe.
+Author     : Bernardo Paulsen
+Version    : 0.0.1
+"""
+
+
+import pickle
+
+
+with open("prices.pickle","rb") as file:
+    df = pickle.load(file)
+
+
+print(len(df.columns))
+df = df[1:]
+df = df.dropna(1)
+print(len(df.columns))
+
+
+with open("cleaned.pickle","wb") as file:
+    pickle.dump(df,file)
